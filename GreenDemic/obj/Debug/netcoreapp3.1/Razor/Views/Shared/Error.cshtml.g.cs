78414c5e0144a8454294516517751b745048ae65
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-11-03 03:19:56 +0800

    Minor changes

--- GreenDemic/obj/Debug/netcoreapp3.1/Razor/Views/Shared/Error.cshtml.g.cs
+++ GreenDemic/obj/Debug/netcoreapp3.1/Razor/Views/Shared/Error.cshtml.g.cs
@@ -26,8 +26,15 @@ using GreenDemic.Models;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 4 "C:\Ngee Ann\2.2\Portfolio development\Final\GreenDemic\GreenDemic\Views\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Http;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d6a5625cc8fb4476f348b0fe9041c550465d8bf9", @"/Views/Shared/Error.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"dfbb355a06f314d32141b553ce9ce483d4f36e6e", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"651a7dca8d4e0d7b2afc66133ee976b2ab8d58e7", @"/Views/_ViewImports.cshtml")]
     public class Views_Shared_Error : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<ErrorViewModel>
     {
         #pragma warning disable 1998
