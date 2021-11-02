8d912ba1738e8dffe8d90490862c80017d7e77f3
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-11-03 03:18:52 +0800

    Add logic and view for maxCal for each user

---
+++ GreenDemic/Models/UserCalories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenDemic.Models
+{
+    public class UserCalories
+    {
+        public string Gender { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public int Age { get; set; }
+        public string ExerciseType { get; set; }
+    }
+}
