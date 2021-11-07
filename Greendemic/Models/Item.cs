9269f47e48a7701fb1b4dad3b7ae3a214e65de99
tayyikyong <74086947+tayyikyong@users.noreply.github.com>
2021-11-07 15:54:35 +0800

    Add project files.

---
+++ Greendemic/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Greendemic.Models
+{
+    public class Item
+    {
+        [Required]
+        [StringLength(10)]
+        public int ItemID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string ItemName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; }
+        [Required]
+        [StringLength(10)]
+        public int Cal { get; set; }
+    }
+}
