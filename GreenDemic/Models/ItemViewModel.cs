2ec483b93d124f894f2721712b096c86fe63b567
Balqis Kinanti <S10204973@connect.np.edu.sg>
2021-11-14 19:59:36 +0800

    Add view model for item

---
+++ GreenDemic/Models/ItemViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenDemic.Models
+{
+    public class ItemViewModel
+    {
+        [Display(Name = "Item ID")]
+        [Required]
+        public int ItemID { get; set; }
+
+        [Display(Name = "Shopping Bag ID")]
+        [Required]
+        public int ShoppingBagID { get; set; }
+
+        [Display(Name = "Item Name")]
+        [Required]
+        [StringLength(50, ErrorMessage = "Item Name cannot exceed 50 characters")]
+        public string ItemName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Category cannot exceed 50 characters")]
+        public string Category { get; set; }
+
+        [Display(Name = "Calories(per item)")]
+        [Required]
+        public int Cal { get; set; }
+
+        [Display(Name = "Quantity")]
+        [Required]
+        public int Qty { get; set; }
+
+        [Display(Name = "Calorie Sub Total")]
+        public int CalSubTotal { get; set; }
+    }
+}
