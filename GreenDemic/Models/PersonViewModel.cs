02ebdf7be22f2ccd5b2db6c0dac6f0e07a415587
Balqis Kinanti <S10204973@connect.np.edu.sg>
2022-01-14 11:53:37 +0800

    Update Person View Model

---
+++ GreenDemic/Models/PersonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenDemic.Models
+{
+    public class PersonViewModel
+    {
+        [Display(Name = "User ID")]
+        public int UserID { get; set; }
+
+        [Display(Name = "Family Member Name")]
+        [StringLength(255, ErrorMessage = "Name cannot exceed 255 characters")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Maximum Calories / day")]
+        public int MaxCal { get; set; }
+
+        [Display(Name = "Maximum Calories / day")]
+        public int DerivedMaxCal { get; set; }
+
+        public int Age { get; set; }
+    }
+}
